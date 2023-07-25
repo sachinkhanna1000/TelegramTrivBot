a26e2b0aac64d3742427c01fe4b3394f9e65f0bb
Sachin Khanna <112667136+sachinkhanna1000@users.noreply.github.com>
2023-07-26 00:25:41 +0800

    Update trivBot.py

--- trivBot.py
+++ trivBot.py
@@ -2,16 +2,12 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import openai
 
-# Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Telegram bot token
 TOKEN = '6236204817:AAF0TKOdox9lzXUpmV_xhnPUAGvzlcZneQM'
 
-# OpenAI API Key
 openai.api_key = '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'
 
-# Function to ask a question using OpenAI
 def ask_question(question):
     chat_history = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -22,11 +18,9 @@ def ask_question(question):
     )
     return chat_history.choices[0].message.content
 
-# Handler for /start command
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm a trivia bot. Feel free to ask me any question!")
 
-# Handler for regular messages
 def handle_message(update, context):
     user_input = update.message.text
     try:
@@ -36,20 +30,15 @@ def handle_message(update, context):
         print("Error:", e)
 
 def main():
-    # Create the Updater and pass it your bot's token
     updater = Updater(TOKEN, use_context=True)
 
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Register handlers
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
-    # Start the bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C
     updater.idle()
 
 if __name__ == '__main__':
